8994ed0b52c950e6c2797ef57a04699108661c5f
MRochaa <98289498+MRochaa@users.noreply.github.com>
2025-09-11 09:53:41 -0300

    Update tabs.tsx

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -7,7 +7,6 @@ import { cn } from "@/lib/utils"
 
 const Tabs = TabsPrimitive.Root
 
-// TabsList - Container para os triggers das abas
 const TabsList = React.forwardRef
   React.ElementRef<typeof TabsPrimitive.List>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
@@ -23,7 +22,6 @@ const TabsList = React.forwardRef
 ))
 TabsList.displayName = TabsPrimitive.List.displayName
 
-// TabsTrigger - Botão individual de cada aba
 const TabsTrigger = React.forwardRef
   React.ElementRef<typeof TabsPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
@@ -39,20 +37,6 @@ const TabsTrigger = React.forwardRef
 ))
 TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
 
-// TabsContent - Conteúdo de cada aba
 const TabsContent = React.forwardRef
   React.ElementRef<typeof TabsPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content
-    ref={ref}
-    className={cn(
-      "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-      className
-    )}
-    {...props}
-  />
-))
-TabsContent.displayName = TabsPrimitive.Content.displayName
-
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+  React.ComponentProps
